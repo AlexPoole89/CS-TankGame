d53d24dbd5fd01132b772b1871a59fef311084b8
Alex Poole <nameless.one89@gmail.com>
2017-10-04 13:29:42 -0400

    *login page

---
+++ tbUI/tbUI/Database.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tbUI
+{
+    class Database
+    {
+        static String connString ="TODO: ADD CONNECTION STRING";
+        SqlConnection conn;
+
+        //open connection string
+        public void OpenConnection()
+        {
+            conn = new SqlConnection(connString);
+            conn.Open();
+        }
+
+
+        //method to execute queries
+        public void ExecuteQueries(string Query_)
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand(Query_, conn);
+               // cmd.Parameters.Add(new SqlParameter("Name", conn));
+                cmd.ExecuteNonQuery();
+            }
+            catch (SqlException ex)
+            {
+                Console.Write("EXCEPTION: " + ex.Message);
+            }
+        }
+        //TODO: INSERT, SELECT STATEMENTS 
+
+
+
+        //method to read data. datareader
+        public SqlDataReader DataReader(string Query_)
+        {
+            SqlCommand cmd = new SqlCommand(Query_, conn);
+            SqlDataReader dr = cmd.ExecuteReader();
+            return dr;
+        }
+    }
+}
